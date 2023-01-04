6d2fbe7604b0b1985567c7949ca19feb123af68b
Rodrigo De la vega <delavegardr@gmail.com>
2023-01-04 17:00:35 -0300

    Add Functions librarie

---
+++ Functions.php
@@ -0,0 +1,33 @@
+<?php
+
+class Functions {
+
+    static public function formatFloat($nro, $cantDec){
+        return number_format($nro, $cantDec);
+    }
+
+    static public function truncFraction( $nro, $cantAfterComa ){
+        //REDONDEO A 2 DECIMALES
+        $factor = pow(10, $cantAfterComa); //pow = potencia, 10 ^ $cantAfterComa
+        $nroAux = round($nro * $factor);
+        $res = $nroAux / $factor;
+        
+        return $res;
+    }
+
+    static public function secondsToHours( $cantSeg ){
+        //LO PASO DE SEGUNDOS A MIN
+        $totalMin = $cantSeg / 60;
+
+        //CALCULO CUANTAS HORAS SON (LA PARTE ENTERA SON LAS HORAS)
+        $horas = ($totalMin / 60);
+        $cantHor = number_format( $horas , 0);
+        
+        $cantMin = $totalMin % 60;
+        
+        $totalHoras = $cantHor + ( $cantMin / 100 );
+        
+        return $totalHoras;
+    }
+
+}
