9b41e9775295acce50a23848456e9fd2c5a66ff9
Rodrigo De la vega <delavegardr@gmail.com>
2023-02-02 12:38:53 -0300

    Agrego nueva forma de mostrar un json

--- Response.php
+++ Response.php
@@ -12,6 +12,12 @@ class Response {
         return json_encode( $data );
     }
 
+	static public function toJsonWithOutStatus( $data ){
+		array_unshift($data,[ "totalRegistros" => count($data) ]);
+
+		return json_encode( $data, JSON_FORCE_OBJECT | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE );
+	}
+
     static public function toXml( $ok, $description, $data ){
         $data = [
             "ok" => $ok,
